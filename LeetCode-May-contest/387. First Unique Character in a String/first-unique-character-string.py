9fe3860726ec53abe0bfa26dc8d85de9c3c5c833
harshavardan manam <vardan.manam@gmail.com>
2020-05-05 11:42:34 -0500

    05052020-v2
    05052020- v2 -- changed the default dict to counter

--- LeetCode-May-contest/387. First Unique Character in a String/first-unique-character-string.py
+++ LeetCode-May-contest/387. First Unique Character in a String/first-unique-character-string.py
@@ -1,11 +1,9 @@
+
 class Solution:
     def firstUniqChar(self, s: str) -> int:
         if len(s)==0:
             return -1
-        table=collections.defaultdict(int)
-        for i in range(len(s)):
-            table[s[i]]+=1
-        
+        table=collections.Counter(s)       
         for i in range(len(s)):           
             if table[s[i]]==1:
                 return i           
